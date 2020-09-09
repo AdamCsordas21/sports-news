7c0090765692a7db23998e95285d446500cae56d
AdamCsordas21 <adamcsordas@hotmail.com>
2020-09-09 22:34:48 +0100

    add paragraph to createArticle function

--- src/scripts.spec.ts
+++ src/scripts.spec.ts
@@ -1,15 +1,16 @@
 import { createArticle } from './scripts';
 
 describe('articles', () => {
-  it('creates an article with title and date', () => {
+  it('creates an article with title, date and paragraph', () => {
     // given
     
     // when
-    const actual = createArticle('news title', 'date')
+    const actual = createArticle('news title', 'date', 'paragraph')
     // then
     const expected = `<article>
       <h2>news title</h2>
       <h3>date</h3>
+      <p>paragraph</p>
     </article>`
     expect(actual).toEqual(expected)
   })
--- src/scripts.ts
+++ src/scripts.ts
@@ -1,6 +1,7 @@
-export function createArticle(title: string, date: string): string {
+export function createArticle(title: string, date: string, paragraph: string): string {
   return `<article>
       <h2>${title}</h2>
       <h3>${date}</h3>
+      <p>${paragraph}</p>
     </article>`
 }
