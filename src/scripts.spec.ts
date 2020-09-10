8c6433f9f18a67dfcc6053ee1ccccb8545c2e738
AdamCsordas21 <adamcsordas@hotmail.com>
2020-09-10 18:56:22 +0100

    accept multiple paragraphs
    Co-authored-by: Osh Slomczynski <karol.slomczynski@gmail.com>

--- src/scripts.spec.ts
+++ src/scripts.spec.ts
@@ -1,16 +1,19 @@
 import { createArticle } from './scripts';
 
 describe('articles', () => {
-  it('creates an article with title, date and paragraph', () => {
+  it('creates an article with title, date and paragraphs', () => {
     // given
     
     // when
-    const actual = createArticle('news title', 'date', 'paragraph')
+    const actual = createArticle('news title', 'date', ['paragraph 1', 'paragraph 2'])
     // then
     const expected = `<article>
       <h2>news title</h2>
       <h3>date</h3>
-      <p>paragraph</p>
+      <section>
+        <p>paragraph 1</p>
+        <p>paragraph 2</p>
+      </section>
     </article>`
     expect(actual).toEqual(expected)
   })
--- src/scripts.ts
+++ src/scripts.ts
@@ -1,7 +1,10 @@
-export function createArticle(title: string, date: string, paragraph: string): string {
+export function createArticle(title: string, date: string, paragraphs: string[]): string {
   return `<article>
       <h2>${title}</h2>
       <h3>${date}</h3>
-      <p>${paragraph}</p>
+      <section>
+        <p>${paragraphs.join(`</p>
+        <p>`)}</p>
+      </section>
     </article>`
 }
