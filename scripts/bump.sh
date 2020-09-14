313d1743865c39bb27456f821b0012e315fedf51
AdamCsordas21 <adamcsordas@hotmail.com>
2020-09-14 19:17:36 +0100

    add pre-commit hook with build

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -3,3 +3,9 @@
 version=$(date +%y%m%d_%H%M%S)
 
 sed "s/{{VERSION}}/$version/g" ./static/index.html > ./docs/index.html
+grep '{{VERSION}}' ./docs/index.html > /dev/null
+if [ "$?" -eq "0" ]; then
+  echo '{{VERSION}} not replaced!'
+  exit 1
+fi
+git add docs/index.html
