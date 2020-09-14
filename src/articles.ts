333a5ab194b09d4680decf35479b60665e16306e
AdamCsordas21 <adamcsordas@hotmail.com>
2020-09-14 20:20:02 +0100

    refactor formatDated

--- src/articles.ts
+++ src/articles.ts
@@ -4,15 +4,18 @@ export interface Article {
   contents: string[]
 }
 
-export function createArticle({ title, date, contents }: Article): string {
-  let formattedDate = new Date(date).toLocaleString('en-GB', {
+function formatDate(date: string): string {
+  return new Date(date).toLocaleString('en-GB', {
     month: 'long',
     day: 'numeric',
     year: 'numeric'
   })
+}
+
+export function createArticle({ title, date, contents }: Article): string {
   return `<article>
       <h2>${title}</h2>
-      <h3><time datetime="${date}">${formattedDate}</time></h3>
+      <h3><time datetime="${date}">${formatDate(date)}</time></h3>
       <section>
         ${contents.map(p => `<p>${p}</p>`).join('')}
       </section>
